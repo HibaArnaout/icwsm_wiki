0bc6f7c3d6f051df622100c1b0d785a27410110c
HibaArnaout <harnaout90@gmail.com>
2022-12-14 18:09:50 +0100

    third

--- mpiproject/settings.py
+++ mpiproject/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mpiproject.settings'
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
