6f8f836b357d71761d3a0d3ea932d38ae849147b
Hiba Arnaout <harnaout@lap-15-75.mpi-inf.mpg.de>
2022-12-14 16:21:22 +0100

    first commit

---
+++ demographics/static/js/script.js
@@ -0,0 +1,22 @@
+function showtrivia() {
+	var btnText = document.getElementById("trivia");
+	var other = document.getElementById("demographics");
+	  if (btnText.style.display == "none") {
+	    btnText.style.display = "block";
+	    other.style.display = "none";
+	  } else {
+	    btnText.style.display = "none";
+	  }
+}
+
+function showdemographics() {
+	var btnText = document.getElementById("demographics");
+	var other = document.getElementById("trivia");
+	  if (btnText.style.display == "none") {
+	    btnText.style.display = "block";
+	    other.style.display = "none";
+	  } else {
+	    btnText.style.display = "none";
+	  }
+}
+
