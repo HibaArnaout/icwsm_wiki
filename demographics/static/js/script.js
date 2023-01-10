73fa5e92a2cca152206c6748afe3856e2a29e826
unknown <lap-adm@lap-15-75.mpi-inf.mpg.de>
2023-01-10 13:52:14 +0100

    adding files

--- demographics/static/js/script.js
+++ demographics/static/js/script.js
@@ -1,5 +1,5 @@
 function showcommunities() {
-	var btnText = document.getElementById("community");
+	var btnText = document.getElementById("bycommunity");
 	var other = document.getElementById("demographics");
 	var otherr = document.getElementById("trivia");
 	  if (btnText.style.display == "none") {
@@ -14,7 +14,7 @@ function showcommunities() {
 function showtrivia() {
 	var btnText = document.getElementById("trivia");
 	var other = document.getElementById("demographics");
-	var otherr = document.getElementById("community");
+	var otherr = document.getElementById("bycommunity");
 	  if (btnText.style.display == "none") {
 	    btnText.style.display = "block";
 	    other.style.display = "none";
@@ -27,7 +27,7 @@ function showtrivia() {
 function showdemographics() {
 	var btnText = document.getElementById("demographics");
 	var other = document.getElementById("trivia");
-	var otherr = document.getElementById("community");
+	var otherr = document.getElementById("bycommunity");
 	  if (btnText.style.display == "none") {
 	    btnText.style.display = "block";
 	    other.style.display = "none";
