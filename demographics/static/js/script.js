a3cd4d72d23ef231606a56d8ff026cda065858ef
Hiba Arnaout <harnaout90@gmail.com>
2023-04-05 03:11:14 +0200

    new update

--- demographics/static/js/script.js
+++ demographics/static/js/script.js
@@ -2,9 +2,11 @@ function showcommunities() {
 	var btnText = document.getElementById("bycommunity");
 	var other = document.getElementById("demographics");
 	var otherr = document.getElementById("trivia");
+	var otherrr = document.getElementById("documentation");
 	  if (btnText.style.display == "none") {
 	    btnText.style.display = "block";
 	    other.style.display = "none";
+	    otherr.style.display = "none";
 	    otherr.style.display = "none";
 	  } else {
 	    btnText.style.display = "none";
@@ -15,9 +17,11 @@ function showtrivia() {
 	var btnText = document.getElementById("trivia");
 	var other = document.getElementById("demographics");
 	var otherr = document.getElementById("bycommunity");
+	var otherrr = document.getElementById("documentation");
 	  if (btnText.style.display == "none") {
 	    btnText.style.display = "block";
 	    other.style.display = "none";
+	    otherr.style.display = "none";
 	    otherr.style.display = "none";
 	  } else {
 	    btnText.style.display = "none";
@@ -28,10 +32,27 @@ function showdemographics() {
 	var btnText = document.getElementById("demographics");
 	var other = document.getElementById("trivia");
 	var otherr = document.getElementById("bycommunity");
+	var otherrr = document.getElementById("documentation");
 	  if (btnText.style.display == "none") {
 	    btnText.style.display = "block";
 	    other.style.display = "none";
 	    otherr.style.display = "none";
+	    otherr.style.display = "none";
+	  } else {
+	    btnText.style.display = "none";
+	  }
+}
+
+function showdocumentation() {
+	var otherrr = document.getElementById("demographics");
+	var other = document.getElementById("trivia");
+	var otherr = document.getElementById("bycommunity");
+	var btnText = document.getElementById("documentation");
+	  if (btnText.style.display == "none") {
+	    btnText.style.display = "block";
+	    other.style.display = "none";
+	    otherr.style.display = "none";
+	    otherrr.style.display = "none";
 	  } else {
 	    btnText.style.display = "none";
 	  }
